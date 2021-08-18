83f092fc59aeb14f7ec815d281c0dda0a37c285c
sammyleenUG <60161031+sammyleenUG@users.noreply.github.com>
2021-08-18 09:18:29 +0300

    Update config.php

--- app/config.php
+++ app/config.php
@@ -1,7 +1,6 @@
 <?php
 
-$channel_id = "UCtfR4gl0uGp6pYMU_dIiT5g";
-$api_key = "AIzaSyDfgXMzksOcFkRe5LVLmTDAXjrv4IClGbo";
+$api_key = "";
 $commonRegions = ["NG","UG","KE","TZ"];
 $region = $commonRegions[rand(0,3)];
 $maxPop = 10;
